a346b50c37d9b9b8fa236392f16ea86b4aca3757
johnson-oragui <johnson.oragui@gmail.com>
2025-12-21 11:03:07 +0100

    feat: conditional passed DATABASES setup

--- license-service/config/settings/ci.py
+++ license-service/config/settings/ci.py
@@ -2,6 +2,8 @@
 Config Settings CI
 """
 
+import os
+
 from config.settings.base import *
 
 DEBUG = False
@@ -12,15 +14,23 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "license_service",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": 5432,
+if os.environ.get("CI") != "true":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "license_service",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
+    }
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
