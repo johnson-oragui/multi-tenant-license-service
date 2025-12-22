1bedd683fa9b07374b4b5ad7c9fc4fe8ca030738
johnson-oragui <johnson.oragui@gmail.com>
2025-12-22 17:52:03 +0100

    feat: Update urlpatterns with activate, deactivate, suspend views

--- license-service/licenses/urls.py
+++ license-service/licenses/urls.py
@@ -4,8 +4,44 @@ License URLs
 
 from django.urls import path
 
-from licenses.views import LicenseProvisionView
+from licenses.views import (
+    LicenseDeactivateView,
+    LicenseProvisionView,
+    LicenseReinstateView,
+    LicenseRevokeView,
+    LicenseValidateView,
+    LicenseSuspendView,
+)
 
 urlpatterns = [
-    path("licenses/", LicenseProvisionView.as_view(), name="license-provision"),
+    path(
+        "licenses/",
+        LicenseProvisionView.as_view(),
+        name="license-provision",
+    ),
+    path(
+        "licenses/validate",
+        LicenseValidateView.as_view(),
+        name="license-validate",
+    ),
+    path(
+        "licenses/deactivate",
+        LicenseDeactivateView.as_view(),
+        name="license-deactivate",
+    ),
+    path(
+        "licenses/<uuid:license_id>/revoke/",
+        LicenseRevokeView.as_view(),
+        name="license-revoke",
+    ),
+    path(
+        "licenses/<uuid:license_id>/reinstate/",
+        LicenseReinstateView.as_view(),
+        name="license-reinstate",
+    ),
+    path(
+        "licenses/<uuid:license_id>/suspend/",
+        LicenseSuspendView.as_view(),
+        name="license-suspend",
+    ),
 ]
