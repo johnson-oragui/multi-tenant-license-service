595b84457599148a3a126437cb785b078577ba7c
johnson-oragui <johnson.oragui@gmail.com>
2025-12-22 17:52:29 +0100

    feat: Add test factories

---
+++ license-service/licenses/tests/factories.py
@@ -0,0 +1,49 @@
+"""
+Test factory
+"""
+
+from django.utils import timezone
+from licenses.models import License, LicenseKey, Product, LicenseActivation
+from licenses.models import LicenseStatus
+
+
+def product_factory(code="PROD-001"):
+    """
+    product factory
+    """
+    return Product.objects.create(code=code, name="Test Product")
+
+
+def license_key_factory(key="TEST-KEY-123"):
+    """
+    license key factory
+    """
+    return LicenseKey.objects.create(key=key)
+
+
+def license_factory(
+    *,
+    product,
+    license_key,
+    status=LicenseStatus.VALID,
+    expires_at=None,
+):
+    """
+    license factory
+    """
+    return License.objects.create(
+        product=product,
+        license_key=license_key,
+        status=status,
+        expires_at=expires_at or (timezone.now() + timezone.timedelta(days=30)),
+    )
+
+
+def activation_factory(*, license, instance_identifier):
+    """
+    activation factory
+    """
+    return LicenseActivation.objects.create(
+        license=license,
+        instance_identifier=instance_identifier,
+    )
